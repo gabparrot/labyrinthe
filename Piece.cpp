3fc6d254e32d552a178dbbef0135bc5f0bfec8bb
gabparrot <gabriel.chevrette-parrot.1@ulaval.ca>
2020-02-08 19:36:35 -0500

    update structure fichier

---
+++ Piece.cpp
@@ -0,0 +1,18 @@
+/**
+ * \file Piece.cpp
+ * \brief Ce fichier contient une implantation des méthodes de la classe Piece
+ * \author Étudiant(e)
+ * \version 0.1
+ * \date janvier 2020
+ *
+ */
+
+#include "pch.h"
+#include "Piece.h"
+
+namespace TP1
+{
+
+	//Mettez l'implémentation de vos méthodes ici.
+
+}
---
+++ Porte.cpp
@@ -0,0 +1,18 @@
+/**
+ * \file Porte.cpp
+ * \brief Ce fichier contient une implantation des méthodes de la classe Porte
+ * \author Étudiant(e)
+ * \version 0.1
+ * \date janvier 2020
+ *
+ */
+
+#include "pch.h"
+#include "Porte.h"
+
+namespace TP1
+{
+
+	//Mettez l'implémentation de vos méthodes ici.
+
+}
